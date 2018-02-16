41a3cd36e58643429a98b8da6375ab2370916daa
Moein <tje3d@yahoo.com>
2018-02-16 15:04:40 +0330

    Fix quote problem

--- app/ResponseParser.php
+++ app/ResponseParser.php
@@ -9,10 +9,10 @@ class ResponseParser
         $output = [];
         $data = str_replace("\'", "'", $data);
 
-        preg_match("/name_enus: '(.*?)(')/s", $data, $match);
+        preg_match("/name_enus: '(.*?)(',)/s", $data, $match);
         $output['name'] = $match[1];
 
-        preg_match("/icon: '(.*?)(')/s", $data, $match);
+        preg_match("/icon: '(.*?)(',)/s", $data, $match);
         $output['icon'] = $match[1];
 
         return $output;
